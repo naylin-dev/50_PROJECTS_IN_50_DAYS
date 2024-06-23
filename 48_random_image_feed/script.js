c9fdf337db3964ea95e7d8070931d23e193470a1
Nay Lin Tun <naylin.ucsy.99@gmail.com>
2024-06-23 12:57:26 +0630

    Update image source URL

--- 48_random_image_feed/script.js
+++ 48_random_image_feed/script.js
@@ -1,17 +1,19 @@
 const container = document.querySelector('.container');
-const unsplashURL = 'https://source.unsplash.com/random/';
+const randomImageURL = 'https://picsum.photos/'; // alternative => https://unsplash.it/
 const rows = 10;
 
 for (let i = 0; i < rows * 3; i++) {
     const img = document.createElement('img');
-    img.src = `${unsplashURL}${getRandomSize()}`;
+    img.src = `${randomImageURL}${getRandomSize()}?random`;
     container.appendChild(img);
 }
 
 function getRandomSize() {
-    return `${getRandomNumber()}x${getRandomNumber()}`;
+    return `${getRandomNumber()}/${getRandomNumber()}`;
 }
 
 function getRandomNumber() {
+    // Generate a random number between 0 and 9.
+    // Then add 300 to it to get a random number between 300 and 309.
     return Math.floor(Math.random() * 10) + 300;
 }
